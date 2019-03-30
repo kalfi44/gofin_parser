e54e49f34523e821b8f5d565e905358f76fa50ee
sareth450 <sareth179@gmail.com>
2019-03-30 11:51:37 +0100

    attachment 3&4 parsed

--- scrapper.rb
+++ scrapper.rb
@@ -3,6 +3,15 @@ require 'httparty'
 require 'byebug'
 require 'open-uri'
 
+def split_and_assemble(str)
+  arr = str.split(' ')
+  chapter_name = " "
+  for i in 1...arr.size
+    chapter_name << (arr[i] + " ")
+  end
+  return arr[0], chapter_name.strip!
+end
+
 def scrapper
   url = 'http://www.przepisy.gofin.pl/przepisy,4,16,194,1276,131381,20190101,rozporzadzenie-ministra-finansow-z-dnia-2032010-r-w-sprawie.html'
   
@@ -36,6 +45,7 @@ def scrapper
 
   end
 
+  #Attachment 1&2 parse
   sec = []
   chap =[]
 
@@ -63,18 +73,52 @@ def scrapper
   end
 
   chap.each do |x|
-    matched_string = x.text.match(/\d{5}\s+(\S*(\s|\p{Zs})*)+/)
+    matched_string = x.text.match(/^[\d]\d{4}\s+(\S*(\s|\p{Zs})*)+/)
     if matched_string != nil
-      arr = matched_string.to_s.split(' ')
-      chapter_name = " "
-      for i in 1...arr.size
-        chapter_name << (arr[i] + " ")
-      end
-      chapters[arr[0]]=chapter_name
+      key,val = split_and_assemble(matched_string.to_s)
+      chapters[key]=val
     end     
   end
+  #Attachment 3 parse
 
-  #byebug
+  par = []
+  paragraphs_i = {}
+  incomes = {}
+  expenses = {}
+  paragraphs_e = {}
+  
+
+  data[2].css('p').each do |x|
+    par.push(x)
+  end
+
+  par.each do |x|
+    if (matched_string = x.text.match(/^[\d]\s+(\S*(\s|\p{Zs})*)+/)) != nil
+      puts matched_string
+      key,val = split_and_assemble(matched_string.to_s)
+      incomes[key]=val
+    elsif (matched_string = x.text.match(/^[\d]\d{2}\s+(\S*(\s|\p{Zs})*)+/)) != nil
+      #puts matched_string
+      key,val = split_and_assemble(matched_string.to_s)
+      paragraphs_i[key]=val
+    end
+  end
+  
+  #Attachment 4 withou groups for now
+  data[3].css('p').each do |x|
+    if (matched_string = x.text.match(/^[\d]\s+(\S*(\s|\p{Zs})*)+/)) != nil
+      puts matched_string
+      key,val = split_and_assemble(matched_string.to_s)
+      expenses[key]=val
+    elsif (matched_string = x.text.match(/^[\d]\d{2}\s+(\S*(\s|\p{Zs})*)+/)) != nil
+      #puts matched_string
+      key,val = split_and_assemble(matched_string.to_s)
+      paragraphs_e[key]=val
+    end
+    
+  end
+   
+  byebug
 end
 
-#scrapper
+scrapper
